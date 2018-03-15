a332c665a19cdd9b6f2455095272d3a7a857b352
DanielDees <explorers2010@hotmail.com>
2018-03-15 02:51:22 -0400

    Fixed issue with capitalized characters not dissappearing.

--- app.js
+++ app.js
@@ -15,20 +15,8 @@ document.onkeyup = getKeyReleased;
 var keyButton = "";
 var str = "";
 
-//Keys Depressed ================
-var keys = {
-
-    //Special
-    ENTER: false,
-    SHIFT: false,
-    DELETE: false,
-
-    //Arrow Keys
-    UP_ARROW: false,
-    DOWN_ARROW: false,
-    LEFT_ARROW: false,
-    RIGHT_ARROW: false,
-};
+//Keys Pressed ================
+var keys = {};
 
 function getKeyReleased (e) {
 
@@ -41,20 +29,23 @@ function getKeyReleased (e) {
   //ENTER KEY (e.which 13 && e.keyCode 13)
   if (e.which == 13 || e.keyCode == 13) { keyButton = "ENTER"; }
   //LEFT ARROW (e.which 37 && e.keyCode 37)
-  if (e.which == 37 || e.keyCode == 37) { keyButton = "LEFT_ARROW"; } 
+  if (e.which == 37 || e.keyCode == 37) { keyButton = "L_ARROW"; } 
   //UP ARROW (e.which 38 && e.keyCode 38)
-  if (e.which == 38 || e.keyCode == 38) { keyButton = "UP_ARROW"; }
+  if (e.which == 38 || e.keyCode == 38) { keyButton = "U_ARROW"; }
   //RIGHT ARROW (e.which 39 && e.keyCode 39)
-  if (e.which == 39 || e.keyCode == 39) { keyButton = "RIGHT_ARROW"; } 
+  if (e.which == 39 || e.keyCode == 39) { keyButton = "R_ARROW"; } 
   //DOWN ARROW (e.which 40 && e.keyCode 40)
-  if (e.which == 40 || e.keyCode == 40) { keyButton = "DOWN_ARROW"; } 
+  if (e.which == 40 || e.keyCode == 40) { keyButton = "D_ARROW"; } 
   //DELETE KEY (e.which 8 && e.keyCode 8)
   if ((e.which == 8 || e.keyCode == 8)) { keyButton = "DELETE"; }
 
   //Show key un-pressed
   //console.log("Un-pressed: " + keyButton);
 
+  //Remove lower case of key
   keys[keyButton] = false;
+  //Remove upper case of key
+  keys[(keyButton.toUpperCase())] = false;
 
   //SHIFT Key
   if (e.which == 16 || e.keyCode == 16) { keys.SHIFT = false; };
@@ -65,7 +56,6 @@ function getKeyReleased (e) {
 }
 
 function getKeyPressed (e) {
-
   return String.fromCharCode(e.which || e.keyCode).toUpperCase();
 }
 function getKeyDown (e) {
@@ -80,26 +70,22 @@ function getKeyDown (e) {
     if (e.keyCode == 8 || (e.keyCode >= 37 && e.keyCode <= 40) || e.keyCode == 16 || e.keyCode == 17 || e.keyCode == 18) { keyClick(e); }
   };
 }
-
 function keyClick (e) {
 
   keyButton = getKeyPressed(e);
 
   //SHIFT KEY (e.shiftkey) || e.which / e.keyCode 16 onKeyDown only
   if (e.which == 16 || e.keyCode == 16) { 
-
     keyButton = "SHIFT"; 
     keys.SHIFT = true; 
   }
   //CTRL Key
   if (e.which == 17 || e.keyCode == 17) { 
-
     keyButton = "ctrl";
     keys.ctrl = true; 
   }
   //ALT Key
   if (e.which == 18 || e.keyCode == 18) { 
-
     keyButton = "alt";
     keys.alt = true;
   }
@@ -111,14 +97,17 @@ function keyClick (e) {
   //ENTER KEY (e.which 13 && e.keyCode 13)
   if (e.which == 13 || e.keyCode == 13) { keyButton = "ENTER"; }
 
+  if (String.fromCharCode(keyButton) != "&") {
+    
+  }
   //UP ARROW (e.which 38 && e.keyCode 38)
-  if (e.which == 38 || e.keyCode == 38) { keyButton = "UP_ARROW"; }
+  if (e.keyCode == 38 && String.fromCharCode(keyButton) == "&") { keyButton = "U_ARROW"; }
   //DOWN ARROW (e.which 40 && e.keyCode 40)
-  else if (e.which == 40 || e.keyCode == 40) { keyButton = "DOWN_ARROW"; } 
+  if (e.keyCode == 40 && String.fromCharCode(keyButton) == "(") { keyButton = "D_ARROW"; } 
   //LEFT ARROW (e.which 39 && e.keyCode 39)
-  else if (e.which == 37 || e.keyCode == 37) { keyButton = "LEFT_ARROW"; } 
+  if (e.keyCode == 37 && String.fromCharCode(keyButton) == "%") { keyButton = "L_ARROW"; } 
   //RIGHT ARROW (e.which 40 && e.keyCode 40)
-  else if (e.which == 39 || e.keyCode == 39) { keyButton = "RIGHT_ARROW"; } 
+  if (e.keyCode == 39 && String.fromCharCode(keyButton) == "'") { keyButton = "R_ARROW"; } 
 
   //DELETE KEY (e.which 8 && e.keyCode 8)
   if ((e.which == 8 || e.keyCode == 8)) { keyButton = "DELETE"; }
@@ -130,6 +119,40 @@ function keyClick (e) {
 
   e.preventDefault();
 }
+
+
+function showKeys() {
+  for (key in keys) { 
+    if (keys[key] == true) { str += key + " + "; } 
+  }
+
+  ctx.fillText("Keys Pressed: " + str, 40, 40);
+  str = "";
+}
+function showKeysArray() {
+
+  var row = 0;
+  var rowKeys = "";
+  var i = 0;
+
+  //Show Keys array
+  for (key in keys) { 
+
+    rowKeys += key + " + ";
+    i++;
+    
+    ctx.fillText("Keys Array: " + rowKeys, 40, 100 + (20 * row));
+    
+    if (i == 3) { 
+      rowKeys = "";
+      row++; 
+      i = 0; 
+    }
+  }
+
+  str = "";
+}
+
 function displayKeysPressed() {
 
   ctx.clearRect(0,0, canvas.width, canvas.height);
@@ -137,10 +160,7 @@ function displayKeysPressed() {
   ctx.fillStyle = "#0FF";
   ctx.font = "18px Arial";
 
-  for (key in keys) { if (keys[key] == true) { str += key + " + "; } }
-
-  ctx.fillText("Keys Pressed: " + str, 40, 40);
-  str = "";
+  showKeys();
 }
 
 //Refresh Rate
